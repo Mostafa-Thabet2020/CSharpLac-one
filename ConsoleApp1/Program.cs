832c5c424462eb3e8147dc6eb3c24fb8a42f1ec2
Mostafa-Thabet2020 <62522809+Mostafa-Thabet2020@users.noreply.github.com>
2021-10-23 21:49:31 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Your first name is : ");
+
+            //string FirstName = Console.ReadLine();
+
+            //Console.WriteLine("Your secound name is :");
+
+            //string LastName = Console.ReadLine();
+
+            //Console.WriteLine("Your name is "+FirstName+" "+LastName);
+
+
+            //Console.WriteLine("First value :");
+
+            int x = 5;
+
+            //Console.WriteLine("Secound value :");
+
+            //int y = Convert.ToInt32(Console.ReadLine());
+
+            //int result = x * y;
+
+            //Console.WriteLine("Result is : "+ result );
+
+            //Console.WriteLine("write value :");
+
+            decimal m = 5;
+
+            int Result =Convert.ToInt32( m * x);
+            Console.WriteLine(Result);
+        }
+    }
+}
